2799bb466ca7fc02e61e4936bf8904d57fe69de0
IT21195402 <99181341+IT21195402@users.noreply.github.com>
2022-03-06 18:01:31 +0000

    Completed...

--- tute01.cpp
+++ tute01.cpp
@@ -1,34 +1,14 @@
-1
-/*Exercise 1 - Calculations
-
-Convert the C program given below which converts a length given in cm to inches to a C++ program.
-
-Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.
-
-e.g.
-
-float data1;
-int data2;
-scanf("%f", &data1); --> std::cin >> data1;
-scanf("%d", &data2); --> std::cin >> data2; 
-You already know that printf() in C is std::cout in C++ e.g.
-
-printf("Hello World") --> std::cout << "Hello World";
-2.54cm = 1 inch
-*/
-
-
 #include<iostream>
-using namespace std;
-int main() 
+int main()
 {
-    float cm, inches;
+	float cm, inches;
 
-    cout<<"Enter a length in cm : ";
-    cin>>cm;
+	std::cout << "Enter a length in cm :";
+	std::cin >> cm;
 
-    inches = cm / 2.54;
+	inches = cm / 2.54; 
 
-    cout<<"Length in inches is" <<inches<<endl;
-    return 0;
-}  
+	std::cout << "lenth in inches is =" << inches << std::endl;
+
+	return 0;
+}
--- tute03.cpp
+++ tute03.cpp
@@ -1,23 +1,19 @@
-/*Exercise 3 - Repeatition
-
-Convert the C program given below which calculates the Factorial of a number that you input from the keyboard to a C++ program.
-
-Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
-
 #include <iostream>
-using namespace std;
-int main(void)
+int main()
 {
- 	int no;
- 	long fac;
- 	
- 	cout<<"Enter a Number : ";
- 	cin>>no;
- 	
- 	fac = 1;
- 	for (int r=no; r >= 1; r--) {
- 	  fac = fac * r;
- }
- 	cout<<"Factorial of :" << no <<"is"<<fac<<endl;
- 	return 0;
+    int no;
+    long fac;
+
+    std::cout<<"Enter a Number : ";
+    std::cin >> no;
+
+    fac = 1;
+    for (int r = no; r >= 1; r--) 
+    {
+        fac = fac * r;
+    }
+
+    std::cout << "Factorial of " << no << " is " << fac;
+
+    return 0;
 }
